2ec94686e1fd9ef846ab57ae08faaf2cc306b22b
Habownia <hubertkrz7@gmail.com>
2023-03-01 19:31:28 +0100

    Scss config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
 	experimental: {
 		appDir: true,
 	},
 	output: 'standalone',
+	sassOptions: {
+		includePaths: [path.join(__dirname, 'sass')],
+	},
 };
 
 module.exports = nextConfig;
