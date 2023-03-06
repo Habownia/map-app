67988971797ec6f2664fb2ebd050dc48fbe28cf9
Habownia <hubertkrz7@gmail.com>
2023-03-06 15:20:20 +0100

    Navbar animations, links

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,17 +1,35 @@
-import style from '../sass/Nav.module.scss';
+import Link from 'next/link';
+
 import { AiOutlineHeatMap } from 'react-icons/ai';
+import style from '../sass/Nav.module.scss';
 
 export default function Nav() {
+	//style
+	const liLink = `${style.link_li} ${style.link}`;
+
 	return (
 		<div className={style.nav}>
-			<div className={style.logo}>
+			<Link href='/map' className={`${style.logo} ${style.link}`}>
 				<AiOutlineHeatMap className={style.logoIcon} />
 				<p>map-app</p>
-			</div>
+			</Link>
+
 			<ul className={style.list}>
-				<li>Mapa</li>
-				<li>Kontakt</li>
-				<li>Coś tam</li>
+				<li>
+					<Link href='/map' className={liLink}>
+						Mapa
+					</Link>
+				</li>
+				<li>
+					<Link href='/' className={liLink}>
+						Kontakt
+					</Link>
+				</li>
+				<li>
+					<Link href='/test' className={liLink}>
+						Test
+					</Link>
+				</li>
 			</ul>
 		</div>
 	);
